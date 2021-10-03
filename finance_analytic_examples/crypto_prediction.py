242a88eec546969fddc9d06ed1dd94db2e26cf78
Erik Autenrieth <e-aut@web.de>
2021-10-03 10:09:03 +0200

    examples folder

--- finance_analytic_examples/crypto_prediction.py
+++ finance_analytic_examples/crypto_prediction.py
@@ -86,7 +86,6 @@ plot_crypto()
 
 
 # Predict Next Day
-
 real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
