3e18c975517836195da0f4ab50d2c0e6b09d030d
DelPierroZ <pierre_zimmermann@gmx.de>
2021-11-23 13:44:45 +0100

    example IF, Merton Class

--- Klassen/stocks.py
+++ Klassen/stocks.py
@@ -5,13 +5,14 @@ import yfinance as yf
 
 from datetime import datetime
 
+
 class Stocks:
-    def __init__(self,wkns:[]=None, names:[]=None, start=None, stop=None):
-        self.wkns    = wkns
-        self.names   = names
-        self.start   = start
-        self.stop    = stop
-        self.df_stocks  = self.read_and_merge()
+    def __init__(self, wkns: [] = None, names: [] = None, start=None, stop=None):
+        self.wkns = wkns
+        self.names = names
+        self.start = start
+        self.stop = stop
+        self.df_stocks = self.read_and_merge()
         self.df_returns = self.returns()
 
     def read_and_merge(self):
@@ -20,13 +21,13 @@ class Stocks:
             stock = yf.Ticker(wkn)
             df = stock.history(start=self.start, end=self.stop)
             df_list.append(df)
-        stocks = pd.concat(df_list, axis=1,keys=self.names)
-        stocks.columns.names = ['Stock Ticker','Stock Info']
+        stocks = pd.concat(df_list, axis=1, keys=self.names)
+        stocks.columns.names = ['Stock Ticker', 'Stock Info']
         return stocks
 
     def returns(self):
         returns = pd.DataFrame()
-        for name in  self.names:
+        for name in self.names:
             returns[name + ' Return'] = self.df_stocks[name]['Close'].pct_change()
 
         return returns
